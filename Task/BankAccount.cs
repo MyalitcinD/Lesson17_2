34d7ca9f2eaaf845496f9381d296cf90b6ff94a3
Денис <mden1996.07@gmail.com>
2024-03-14 17:02:09 +0300

    Добавьте файлы проекта.

---
+++ Task/BankAccount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    class BankAccount<T>
+    {
+        T accountNumber;
+        double accountBalance;
+        string accountFullName;
+
+        public BankAccount()
+        { }
+        public BankAccount(T AccountNumber, string AccountFullName, double AccountBalance)
+        {
+            accountNumber = AccountNumber;
+            accountFullName = AccountFullName;
+            accountBalance = AccountBalance;
+        }
+        public void setAccNumber(T AccNumber)
+        {
+            this.accountNumber = AccNumber;
+        }
+        public T getAccNumber() {
+            return accountNumber;
+        }
+        public void setAccBalance(double AccBalance)
+        {
+            this.accountBalance = AccBalance;
+        }
+        public double getAccBalance()
+        {
+            return accountBalance;
+        }
+        public void setAccName(string AccFullName)
+        {
+            this.accountFullName = AccFullName;
+        }
+        public string getAccName()
+        {
+            return accountFullName;
+        }
+        public string GetInfo()
+        {
+            return $"Номер акканта: {accountNumber} / ФИО: {accountFullName} / Баланс : {accountBalance}";
+        }
+    }
+}
---
+++ Task/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите числовой номер аккаунта: ");
+            int accNumInt =Convert.ToInt32( Console.ReadLine());
+            Console.Write("Введите ФИО: ");
+            string accFullName = Console.ReadLine();
+            Console.Write("Введите баланс аккаунта: ");
+            double accBalance = Convert.ToDouble(Console.ReadLine());
+            BankAccount<int> bankAccInt = new BankAccount<int>(accNumInt, accFullName, accBalance);
+
+            Console.Write("Введите строковый номер аккаунта: ");
+            string accNumStr = Console.ReadLine();
+            Console.Write("Введите ФИО: ");
+            accFullName = Console.ReadLine();
+            Console.Write("Введите баланс аккаунта: ");
+            accBalance = Convert.ToDouble(Console.ReadLine());
+
+            BankAccount<string> bankAccStr = new BankAccount<string>();
+            bankAccStr.setAccNumber( accNumStr);
+            bankAccStr.setAccName( accFullName);
+            bankAccStr.setAccBalance( accBalance);
+
+            Console.WriteLine();
+            Console.WriteLine(bankAccInt.GetInfo());
+            Console.WriteLine(bankAccStr.GetInfo());
+
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
